42c0f110865273f32fd1e9f7b0558e88ec868ddb
TzuHanLiang <crocodilestear@gmail.com>
2019-10-03 14:34:33 +0800

    iframe: modified

--- build/frontend/assets/js/customs/monitor_main.js
+++ build/frontend/assets/js/customs/monitor_main.js
@@ -1,15 +1,11 @@
-
-
 function onOpen(evt) {
     state.className = "success";
     state.innerHTML = "Connected to server";
-    // 告訴server 我是主螢幕
 }
 
 function onClose(evt) {
     state.className = "fail";
     state.innerHTML = "Not connected";
-    // connected.innerHTML = "0";
 }
 
 function onMessage(evt) {
@@ -33,45 +29,46 @@ function onMessage(evt) {
 }
 
 
-
 const slideplayer = document.querySelector('.slide-player');
 const positioning = document.querySelector('.positioning');
 
-slideplayer.addEventListener('click', () => {
-    if (window.location.href.split('/').pop() === 'slider-main.html') {
-        playSlide();
-        websocket.send(JSON.stringify({
-            event: 'playSlide'
-        }));
-    } else {
-        // 要在按一次play才會play
-        websocket.send(JSON.stringify({
-            event: 'changePage',
-            currentPage: 'index.html',
-        }));
-    }
-}, false);
+if (slideplayer)
+    slideplayer.addEventListener('click', () => {
+        if (window.location.href.split('/').pop() === 'slider-main.html') {
+            playSlide();
+            websocket.send(JSON.stringify({
+                event: 'playSlide'
+            }));
+        } else {
+            // 要在按一次play才會play
+            websocket.send(JSON.stringify({
+                event: 'changePage',
+                currentPage: 'index.html',
+            }));
+        }
+    }, false);
 
-positioning.addEventListener('click', (evt) => {
-    const slideWrap = document.querySelector('.slide-wrap');
-    // console.log(slideWrap.dataset.length, slideWrap.dataset.index);
-    // console.log(slideWrap.dataset.length > 1);
-    // console.log(parseInt(slideWrap.dataset.index) < slideWrap.dataset.length);
-    if (slideWrap.dataset.length > 1 && parseInt(slideWrap.dataset.index) < slideWrap.dataset.length) {
-        websocket.send(JSON.stringify({
-            event: 'changeImage',
-            index: slideWrap.dataset.index,
-        }));
-        slideWrap.dataset.index = parseInt(slideWrap.dataset.index) + 1;
-    } else {
-        websocket.send(JSON.stringify({
-            event: 'changePage',
-            // currentPage: window.location.href,
-            currentPage: window.location.href.split('/').pop(),
-        }));
-        display(window.location.href.split('/').pop(), 'main');
-    }
-}, false);
+if (positioning)
+    positioning.addEventListener('click', (evt) => {
+        const slideWrap = document.querySelector('.slide-wrap');
+        // console.log(slideWrap.dataset.length, slideWrap.dataset.index);
+        // console.log(slideWrap.dataset.length > 1);
+        // console.log(parseInt(slideWrap.dataset.index) < slideWrap.dataset.length);
+        if (slideWrap.dataset.length > 1 && parseInt(slideWrap.dataset.index) < slideWrap.dataset.length) {
+            websocket.send(JSON.stringify({
+                event: 'changeImage',
+                index: slideWrap.dataset.index,
+            }));
+            slideWrap.dataset.index = parseInt(slideWrap.dataset.index) + 1;
+        } else {
+            websocket.send(JSON.stringify({
+                event: 'changePage',
+                // currentPage: window.location.href,
+                currentPage: window.location.href.split('/').pop(),
+            }));
+            display(window.location.href.split('/').pop(), 'main');
+        }
+    }, false);
 
 
 // window.addEventListener('click', (e) => getClickPosition(e, positioning), false);
