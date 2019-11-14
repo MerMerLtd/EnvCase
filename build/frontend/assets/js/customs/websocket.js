d2ac1d85cae6ff16db9d0252830aebce641f8d8b
Luphia Chang <luphia@mermer.cc>
2019-11-14 12:12:52 +0800

    Fix websocket path

--- build/frontend/assets/js/customs/websocket.js
+++ build/frontend/assets/js/customs/websocket.js
@@ -1,40 +1,39 @@
 
 // Query DOM
-var state = document.getElementById("status"),
-    connected = document.getElementById("connected"),
-    log = document.getElementById("log");
+let state = document.getElementById('status');
+    var connected = document.getElementById("connected");
+    var log = document.getElementById('log');
 
 if (window.WebSocket === undefined) {
-    state.innerHTML = "sockets not supported";
-    state.className = "fail";
+  state.innerHTML = 'sockets not supported';
+  state.className = 'fail';
 } else {
-    if (typeof String.prototype.startsWith != "function") {
-        String.prototype.startsWith = function (str) {
-            return this.indexOf(str) == 0;
-        };
-    }
-    window.addEventListener("load", onLoad, false);
+  if (typeof String.prototype.startsWith !== 'function') {
+    String.prototype.startsWith = function (str) {
+      return this.indexOf(str) == 0;
+    };
+  }
+  window.addEventListener('load', onLoad, false);
 }
 
 function onLoad() {
-    var wsUri = "ws://127.0.0.1:80";
-    websocket = new WebSocket(wsUri);
-    websocket.onopen = function (evt) {
-        onOpen(evt)
-    };
-    websocket.onclose = function (evt) {
-        onClose(evt)
-    };
-    websocket.onmessage = function (evt) {
-        onMessage(evt)
-    };
-    websocket.onerror = function (evt) {
-        onError(evt)
-    };
+  let wsUri = `ws://${location.hostname}`;
+  websocket = new WebSocket(wsUri);
+  websocket.onopen = function (evt) {
+    onOpen(evt);
+  };
+  websocket.onclose = function (evt) {
+    onClose(evt);
+  };
+  websocket.onmessage = function (evt) {
+    onMessage(evt);
+  };
+  websocket.onerror = function (evt) {
+    onError(evt);
+  };
 }
 
 function onError(evt) {
-    state.className = "fail";
-    state.innerHTML = "Communication error";
+  state.className = 'fail';
+  state.innerHTML = 'Communication error';
 }
-
